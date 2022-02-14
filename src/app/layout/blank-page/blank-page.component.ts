67ca4098b30d569ac9ef16e817f888815bd71d1d
NgocTien <dangngoctien98@gmail.com>
2022-02-14 10:14:03 +0700

    first commit

---
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-blank-page',
+    templateUrl: './blank-page.component.html',
+    styleUrls: ['./blank-page.component.scss']
+})
+export class BlankPageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+
+    movies: Movie[] =[
+ 
+        {title:'Zootopia',director:'Byron Howard, Rich Moore',cast:'Idris Elba, Ginnifer Goodwin, Jason Bateman',releaseDate:'March 4, 2016'},
+        {title:'Batman v Superman: Dawn of Justice',director:'Zack Snyder',cast:'Ben Affleck, Henry Cavill, Amy Adams',releaseDate:'March 25, 2016'},
+        {title:'Captain American: Civil War',director:'Anthony Russo, Joe Russo',cast:'Scarlett Johansson, Elizabeth Olsen, Chris Evans',releaseDate:'May 6, 2016'},
+        {title:'X-Men: Apocalypse',director:'Bryan Singer',cast:'Jennifer Lawrence, Olivia Munn, Oscar Isaac',releaseDate:'May 27, 2016'},
+        {title:'Warcraft',director:'Duncan Jones',cast:'Travis Fimmel, Robert Kazinsky, Ben Foster',releaseDate:'June 10, 2016'},
+      ]
+
+      employees = [
+        {
+          name: "Rahul", email: "rahul@gmail.com",
+          skills: [{ skill: 'Angular', exp: '2' },{ skill: 'Javascript', exp: '7' },{ skill: 'TypeScript', exp: '3' }
+          ]
+        },
+        {
+          name: "Sachin", email: "sachin@gmail.com",
+          skills: [{ skill: 'Angular', exp: '1' },{ skill: 'Android', exp: '3' },{ skill: 'React', exp: '2' }
+          ]
+        },
+        {
+          name: "Laxman", email: "laxman@gmail.com",
+          skills: [{ skill: 'HTML', exp: '2' },{ skill: 'CSS', exp: '2' },{ skill: 'Javascript', exp: '1' }
+          ]
+        }
+      ]
+    
+}
+class Movie {
+    title : string;
+    director : string;
+    cast : string;
+    releaseDate : string;
+  }
